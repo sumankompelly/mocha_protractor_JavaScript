bd5d4f9cce2dc308d0e2dbdb924686333399c93e
Suman Kompelly <suman.kompelly@odsmed.com>
2020-01-30 22:23:21 -0500

    added code for entering next week date

--- AutomationTestWithMocha/test/appointmentrequest.js
+++ AutomationTestWithMocha/test/appointmentrequest.js
@@ -8,6 +8,15 @@ describe('appointment' , function() {
     assert(browser.getTitle().toEqual('El Guntors'));
     var appointmentRequest = element(by.xpath("//a[@id = 'AtAppointmentLink']/h2[contains(.,'Appointment request')]")).click();
     var phone = element(by.xpath("//label[@class = 'global-services__label']/span[contains(.,'Phone')]")).click();
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+     today = mm + '/' + dd + '/' + yyyy;
+     document.write(today);
+     var nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+     element(by.id("choosenDate")).sendKeys(nextWeek);
+     element(by.id('choosenTime')).sendKeys('10 AM')
     element(by.id('name')).sendKeys('suman');
     element(by.id('email')).sendKeys('suman@gmail.com');
     element(by.id('autoSubscribe')).click();
