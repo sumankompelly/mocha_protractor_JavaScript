c43621bb0cc5b6ebb03be8971912ef808a89523b
Suman Kompelly <suman.kompelly@odsmed.com>
2020-01-30 21:42:21 -0500

    added files

--- AutomationTestWithMocha/appointmentrequest.js
+++ AutomationTestWithMocha/appointmentrequest.js
@@ -1,12 +1,18 @@
+import { browser, element, by } from "protractor"
 
+var assert = chai.assert;
+
+describe('appointment' , function() {
     it('appointment request',() => {
-           var appointmentRequest = element(by.xpath("//a[@id = 'AtAppointmentLink']/h2[contains(.,'Appointment request')]")).click();
-    });
-
-    it('phone select',() => {
-         var phone = element(by.xpath("//label[@class = 'global-services__label']/span[contains(.,'Phone')]")).click();
-    });
-
-    
-
+    browser.get('https://elguntors-stg.salesfloor.net/reggie');
+    assert(browser.getTitle().toEqual('El Guntors'));
+    var appointmentRequest = element(by.xpath("//a[@id = 'AtAppointmentLink']/h2[contains(.,'Appointment request')]")).click();
+    var phone = element(by.xpath("//label[@class = 'global-services__label']/span[contains(.,'Phone')]")).click();
+    element(by.id('name')).sendKeys('suman');
+    element(by.id('email')).sendKeys('suman@gmail.com');
+    element(by.id('autoSubscribe')).click();
+    element(by.id('phone')).sendKeys('5144493882');
+    element(by.name('extraInfo')).sendKeys('need to have a appointment');
+    element(by.xpath("//button[contains(.,'Send Request')]")).click();
+  });   
 });
